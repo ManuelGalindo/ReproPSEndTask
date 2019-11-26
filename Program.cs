dd313c30671e5211dc1e823bb80ac0ff900833fe
Manuel Galindo ⛰️ <magalin@microsoft.com>
2019-11-26 10:55:23 -0600

    repro

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            while (true)
+            {
+                Thread.Sleep(10);
+            }
+
+            // ReSharper disable once FunctionNeverReturns
+        }
+    }
+}
